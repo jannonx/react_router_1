10476108d2bd340367753dedd574717aeaa04b62
jannonx <791273579@qq.com>
2019-12-01 00:48:02 +0800

    动态传值-2 to跳转

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom"
+
+
 class Index extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            list: [
+                { cid: 123, title: "janonx的个人博客-1" },
+                { cid: 234, title: "janonx的个人博客-2" },
+                { cid: 456, title: "janonx的个人博客-3" },
+            ]
+        }
     }
     render() {
         return (
-            <h2>jannon.com</h2>
+            <div>
+                <h2>jannon.com</h2>
+                <ul>
+                    {
+                        this.state.list.map((item, index) => {
+                            return (
+                                <li key={index}>
+                                    <Link to={'/list/' + item.cid}>
+                                        {item.title}
+                                    </Link>
+
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+
         );
     }
 }
