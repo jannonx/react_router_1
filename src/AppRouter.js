f382237df355500915e56fb87317891ebe0cd414
jannonx <791273579@qq.com>
2019-12-01 00:21:31 +0800

    first commit

---
+++ src/AppRouter.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route ,Link} from 'react-router-dom'
+import Index  from './Pages/Index'
+import List  from './Pages/List'
+
+
+function AppRouter() {
+    return (
+        <Router>
+            <ul>
+                <li><Link to="/">首页</Link></li>
+                <li><Link to="/list/">列表</Link></li>
+            </ul>
+
+            <Route path="/" exact component={Index} />
+            <Route path="/list/" component={List} />
+        </Router>
+    )
+}
+
+export default AppRouter;
---
+++ src/Pages/List.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class List extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <h2>List_page</h2>
+        );
+    }
+}
+
+export default List;
